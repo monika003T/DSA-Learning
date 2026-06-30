c745303bc9f947d6c76f6648411b0eeac0def55d
monika003T <monikathakur2062@gmail.com>
2026-06-30 15:30:09 +0530

    traverse array

---
+++ revision/practice/allArr.java
@@ -0,0 +1,10 @@
+package revision.practice;
+
+public class allArr {
+    public static void main(String[] args) {
+        int[] arr={3,4,5,7,2};
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        System.out.println("Index " +i+ " = "+arr[i]);
+    }
+}
