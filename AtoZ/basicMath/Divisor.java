6141a658b4b75607ad398dd2bbaa70e79cb72354
monika003T <monikathakur2062@gmail.com>
2026-07-15 09:57:27 +0530

    Divisor Optimized

--- AtoZ/basicMath/Divisor.java
+++ AtoZ/basicMath/Divisor.java
@@ -1,8 +1,10 @@
 package basicMath;
 import java.util.ArrayList;
+import java.util.Collections;
 public class Divisor {
     public static void main(String[] args) {
         System.out.println(DivisorsOfNumber(36));
+        System.out.println(DivisorOpti(49));
         
     }
     public static ArrayList<Integer> DivisorsOfNumber(int n){
@@ -12,6 +14,19 @@ public class Divisor {
                 List.add(i);
             }
         }
+        return List;
+    }
+    public  static ArrayList<Integer> DivisorOpti(int n){
+         ArrayList<Integer> List =new ArrayList<>();
+        for(int i=1; i*i<=n; i++){
+            if(n%i==0){
+                List.add(i);
+                if(i!=n/i){
+                    List.add(n/i);
+                }
+            }
+        }
+        Collections.sort(List);
         return List;
     }
 }
